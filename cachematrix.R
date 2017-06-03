7c0ab563dfb79627f95afa93834c26a0318426d7
Wally Talen <wally@talen.com>
2017-06-02 21:57:21 -0500

    R Programming Assignment 2
    R Programming - Week 3
    Programming Assignment 2
    Initial submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##
+# R Programming - Week 3
+# Programming Assignment 2
+#
+# Find the inverse of an invertible (nonsingular) square matrix. Cache the
+# result for duplicate calls to solve for the inverse.
+#
+# We assume that the matrix is always invertible
+#
+# To test type the following statements in RStudio:
+#
+#       aMatrix <- matrix(c(4,3,3,2),nrow=2,ncol=2) # create 2x2 matrix
+#       aMakeMatrix <- makeCacheMatrix(aMatrix)     # create caching object
+#       aMatrixinv <- cacheSolve(aMakeMatrix)       # calculate the inverse
+#       aMatrixinv <- cacheSolve(aMakeMatrix)       # use the cached inverse
+#       aMatrix %*% aMatrixinv                      # identity matrix
+#
+# makeCachMatrix - instantiate a matrix caching object in the 
+#                  R environment
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get    <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list ( set = set, 
+           get = get, 
+           setinv = setinv, 
+           getinv = getinv
+         )
 }
-
-
-## Write a short comment describing this function
-
+#
+# cacheSolve - determines the inverse of a square matrix.
+#              If the matrix has already been calculated
+#              it returns the cached value.
+#
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if ( !is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setinv(m)
+    m
 }
